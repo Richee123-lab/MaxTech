a4abe6a2d65fd2cdfe1eeab7b4aaf89739e3ea19
Richee123-lab <chibuikekelechi04@gmail.com>
2026-07-11 17:22:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 server.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("MaxTech Backend is running successfully!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
